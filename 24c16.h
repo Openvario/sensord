bca488103525f6862d18e45ee51c07c06022cc9c
Timo Bruderek <timo.bruderek@gmx.de>
2014-09-29 05:00:50 +0000

    Added EEPROM support functions

---
+++ 24c16.h
@@ -0,0 +1,26 @@
+#define EEPROM_ADR 0x50
+
+#define EEPROM_DATA_VERSION 1
+
+// define struct for MS5611 sensor
+typedef struct {
+	int fd;
+	unsigned char address;
+} t_24c16;
+
+typedef struct {
+	char header[3];
+	char data_version;
+	char serial[6];
+	float zero_offset;
+	char checksum;
+} t_eeprom_data;
+
+// prototypes
+int eeprom_open(t_24c16 *, unsigned char);
+char eeprom_write(t_24c16 *, char *, unsigned char, unsigned char);
+char eeprom_read(t_24c16 *, char *, char, char);
+int update_checksum(t_eeprom_data*);
+char verify_checksum(t_eeprom_data*);
+
+
